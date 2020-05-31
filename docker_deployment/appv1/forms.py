5a246539896c80dffd3ca633c7103f3a285c9ce9
luisantoniococa <luicocurd@gmail.com>
2020-05-31 18:50:13 -0500

    added the files nessesary for a deployment in google kubernetis engine

---
+++ docker_deployment/appv1/forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField
+
+class PredictForm(FlaskForm):
+    reason = StringField('reason')
+    month = StringField('month')
+    transportation = StringField('transportation')
+    age = StringField('age')
+    body = StringField('body')
+    education = SelectField(label='education', choices=['GED', 'Undergrad', 'Graduate', 'PhD'])
+    children = StringField('children')
+    pets = StringField('pets')    
+    submit = SubmitField('predict')
